8cb3d43c293065600636a3f56784deec8f1233d0
Christopher Strauss <cstrauss.mmt-b2011@fh-salzburg.ac.at>
2015-04-21 00:57:23 +0200

    Function PI + Number in PI

--- modules/serverMethods.js
+++ modules/serverMethods.js
@@ -74,6 +74,25 @@ var nextHigherPrimeNumber = function(number){
   return tmp;
 }
 
+var calcPI = function(number){
+  var Pi=0;
+  var n=1;
+  
+  for (i=0;i<=number;i++)
+  {
+    Pi=Pi+(4/n)-(4/(n+2))
+    n=n+4
+  }
+
+  return Pi;
+}
+
+var findNumberInPI = function(number){
+  var pi = calcPI(1000).toString();
+  return pi.indexOf(number.toString()) - 2;
+}
+
+
 module.exports = {
   isPrimeNumber: isPrimeNumber,
   nextHigherPrimeNumber: nextHigherPrimeNumber,
