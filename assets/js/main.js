d796f4d93fe140de315be34ab7ce36aa5e24e2ab
Lukas Wanko <lwanko.mmt-m2014@fh-salzburg.ac.at>
2015-04-20 23:18:20 +0200

    improved design

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,43 +3,18 @@ var app = angular.module('kss', [])
 angular.module('kss').controller('MainCtrl', function ($scope, $http) {
   $scope.lookup = function () {
 
-    $http.get("/number", {
+    $http.get("/query", {
       params: {
         "number": $scope.number
       }
-    }).
-      success(function (data) {
-        console.log(data);
+    }).success(function (data) {
+      $scope.results = data;
+    }).error(function (data, status) {
+      $scope.data = data || "Request failed";
+      $scope.status = status;
+      console.log("error", status, data);
+    });
 
-        $scope.results = JSON.parse(data);
-        $scope.results = {
-          "fibonacci": {
-            "time": "1",
-            "result": "",
-            "description": ""
-          },
-          "prime": {
-            "time": "2",
-            "result": "",
-            "description": ""
-          },
-          "next-prime": {
-            "time": "3",
-            "result": "",
-            "description": ""
-          },
-          "prime-factors": {
-            "time": "4",
-            "result": "",
-            "description": ""
-          }
-        };
-      })
-      .error(function (data, status) {
-        $scope.data = data || "Request failed";
-        $scope.status = status;
-        console.log("error", status, data);
-      });
   };
 });
 
